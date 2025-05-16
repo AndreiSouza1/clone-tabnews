e9f275f6140aeb3fb45f1c709642edd7af0343db
LoSTsc <59926056+LoSTsc@users.noreply.github.com>
2025-05-16 11:34:20 +0000

    Atualização de mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Aio Aio Aio, Luany se você me ama imite um papagaio🦜</h1>
+    return <h1>Aio Aio Aio, Luany se você me ama imite um cavalo🐎</h1>
 }
 
 export default Home;
